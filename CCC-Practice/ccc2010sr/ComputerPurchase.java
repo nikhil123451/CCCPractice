16caf1261b4f479e57bbc7639393bbc4cea864d3
nikti <nikti@Andy>
2025-06-19 09:31:25 -0600

    removed dead code

--- CCC-Practice/ccc2010sr/ComputerPurchase.java
+++ CCC-Practice/ccc2010sr/ComputerPurchase.java
@@ -1,7 +1,6 @@
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class ComputerPurchase {
 	static Scanner scn = new Scanner(System.in);
