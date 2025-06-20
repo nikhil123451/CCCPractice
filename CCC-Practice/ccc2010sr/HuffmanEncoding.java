833a020c101764034a9a3777d46804f6a3529b68
nikti <nikti@Andy>
2025-06-20 17:50:23 -0600

    registering characters and binary values works

--- CCC-Practice/ccc2010sr/HuffmanEncoding.java
+++ CCC-Practice/ccc2010sr/HuffmanEncoding.java
@@ -1,3 +1,28 @@
-public class HuffmanEncoding {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class HuffmanEncoding {
+	static Scanner scn = new Scanner(System.in);
+	
+	public static void main(String args[]) {
+		int numberOfCharactersToEncode;
+		System.out.print("");
+		numberOfCharactersToEncode = scn.nextInt();
+		
+		if (numberOfCharactersToEncode >= 1 && numberOfCharactersToEncode <= 20) {
+			String[] characters = new String[numberOfCharactersToEncode];
+			String[] binaryData = new String[numberOfCharactersToEncode];
+			
+			for (int i = 0 ; i < numberOfCharactersToEncode ; i++) {
+				System.out.print("");
+				String character = scn.next();
+				String binaryValue = scn.next();
+				
+				characters[i] = character;
+				binaryData[i] = binaryValue;
+			}
+			System.out.println(Arrays.toString(characters));
+			System.out.println(Arrays.toString(binaryData));
+		}
+	}
 }
