04006751f6eb2e5dd08306de84db4b70340ddfbc
nikti <nikti@Andy>
2025-07-02 21:06:55 -0600

    added an if statement to account for the input spec

--- CCC-Practice/ccc2010jr/WhatIsNDaddy.java
+++ CCC-Practice/ccc2010jr/WhatIsNDaddy.java
@@ -7,16 +7,18 @@ public class WhatIsNDaddy {
 	public static void main(String args[]) {
 		System.out.print("");
 		int inputNumber = scn.nextInt();
-		int total = 0;
-		for (int i = 0 ; i < NUMBERS_PER_HAND.length ; i++) {
-			int currentFirstNumber = NUMBERS_PER_HAND[i];
-			for (int j = 0 ; j < NUMBERS_PER_HAND.length ; j++) {
-				int currentSecondNumber = NUMBERS_PER_HAND[j];
-				if (currentFirstNumber + currentSecondNumber == inputNumber) {
-					total++;
+		if (inputNumber >= 1 && inputNumber <= 10) {
+			int total = 0;
+			for (int i = 0 ; i < NUMBERS_PER_HAND.length ; i++) {
+				int currentFirstNumber = NUMBERS_PER_HAND[i];
+				for (int j = 0 ; j < NUMBERS_PER_HAND.length ; j++) {
+					int currentSecondNumber = NUMBERS_PER_HAND[j];
+					if (currentFirstNumber + currentSecondNumber == inputNumber) {
+						total++;
+					}
 				}
 			}
+			System.out.println(total);
 		}
-		System.out.println(total);
 	}
 }
