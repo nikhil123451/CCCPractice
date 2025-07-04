ba562ba4f1c5853695b5547dd5080aca6d1b63b9
nikti <nikti@Andy>
2025-07-04 10:45:09 -0600

    works

--- CCC-Practice/ccc2010jr/UpAndDown.java
+++ CCC-Practice/ccc2010jr/UpAndDown.java
@@ -19,11 +19,58 @@ public class UpAndDown {
 				(nikkyStepsBackward >= 1 && nikkyStepsBackward <= 100) &&
 				(byronStepsForward >= 1 && byronStepsForward <= 100) &&
 				(byronStepsBackward >= 1 && byronStepsBackward <= 100)) {
-				System.out.println(nikkyStepsForward);
-				System.out.println(nikkyStepsBackward);
-				System.out.println(byronStepsForward);
-				System.out.println(byronStepsBackward);
-				System.out.println(totalSteps);
+				
+				int nikkyDisplacement = 0;
+				int byronDisplacement = 0;
+				int nikkyStepCounter = totalSteps;
+				int byronStepCounter = totalSteps;
+				
+				while (nikkyStepCounter > 0) {
+					if (nikkyStepsForward <= nikkyStepCounter) {
+						nikkyDisplacement += nikkyStepsForward;
+					} else {
+						nikkyStepsForward = nikkyStepCounter;
+						nikkyDisplacement += nikkyStepsForward;
+					}
+					nikkyStepCounter -= nikkyStepsForward;
+					
+					if (nikkyStepCounter > 0) {
+						if (nikkyStepsBackward <= nikkyStepCounter) {
+							nikkyDisplacement -= nikkyStepsBackward;
+						} else {
+							nikkyStepsBackward = nikkyStepCounter;
+							nikkyDisplacement -= nikkyStepsBackward;
+						}
+						nikkyStepCounter -= nikkyStepsBackward;
+					}
+				}
+				while (byronStepCounter > 0) {
+					if (byronStepsForward <= byronStepCounter) {
+						byronDisplacement += byronStepsForward;
+					} else {
+						byronStepsForward = byronStepCounter;
+						byronDisplacement += byronStepsForward;
+					}
+					byronStepCounter -= byronStepsForward;
+					
+					if (byronStepCounter > 0) {
+						if (byronStepsBackward <= byronStepCounter) {
+							byronDisplacement -= byronStepsBackward;
+						} else {
+							byronStepsBackward = byronStepCounter;
+							byronDisplacement -= byronStepsBackward;
+						}
+						byronStepCounter -= byronStepsBackward;
+					}
+				}
+				
+				if (nikkyDisplacement > byronDisplacement) {
+					System.out.println("Nikky");
+				} else if (nikkyDisplacement == byronDisplacement) {
+					System.out.println("Tied");
+				} else {
+					System.out.println("Byron");
+				}
 			}
 		}
 	}
