fbc98ecd1650e36f2fc4749a577f30505f4c2d53
nikti <nikti@Andy>
2025-07-13 10:04:27 -0600

    works and understood

--- CCC-Practice/ccc2011jr/Unfriend.java
+++ CCC-Practice/ccc2011jr/Unfriend.java
@@ -4,46 +4,57 @@ public class Unfriend { //taken from GPT and modified
 	
     static Scanner scn = new Scanner(System.in); //creating a scanner to read input from console
 	
-    public static void main(String[] args) {
-        int N = scn.nextInt();
+    public static void main(String[] args) { //main method
+        int peopleInNetwork = scn.nextInt(); //getting the amount of people in the network
 
-        List<Integer>[] tree = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++) tree[i] = new ArrayList<>();
-
-        for (int i = 1; i < N; i++) {
-            int inviter = scn.nextInt();
-            tree[inviter].add(i);
+        List<Integer>[] tree = new ArrayList[peopleInNetwork + 1]; //creating a list of arrays to represent a tree with peopleInNetwork + 1 cuz java is 0-indexed
+        for (int i = 1; i <= peopleInNetwork; i++) { //looping through all the people in the network
+        	tree[i] = new ArrayList<>(); //initializing every element in the list with an empty arrayList
         }
 
-        int count = 0;
+        for (int i = 1; i < peopleInNetwork; i++) { //looping through every person in the network minus mark
+            int inviter = scn.nextInt(); //get the ID for the person invited through input
+            tree[inviter].add(i); //add the inviter to the tree
+        }
 
-        for (int mask = 0; mask < (1 << (N - 1)); mask++) {
-            Set<Integer> removed = new HashSet<>();
-            for (int i = 1; i < N; i++) {
-                if ((mask & (1 << (i - 1))) != 0) removed.add(i);
-            }
+        int count = 0; //initializing a counter to keep track of all possible sets of people that can be removed
 
-            boolean valid = true;
-            for (int person : removed) {
-                Stack<Integer> stack = new Stack<>();
-                stack.push(person);
-                while (!stack.isEmpty()) {
-                    int current = stack.pop();
-                    for (int child : tree[current]) {
-                        if (!removed.contains(child)) {
-                            valid = false;
-                            break;
-                        }
-                        stack.push(child);
-                    }
-                    if (!valid) break;
+        for (int subset = 0; subset < (1 << (peopleInNetwork - 1)); subset++) { //looping through all the subsets (1 << (peopleInNetwork - 1)) = 2^(peopleInNetwork - 1)
+            Set<Integer> removedPeople = new HashSet<>(); //creating a hashSet to keep track of all people removed
+            for (int i = 1; i < peopleInNetwork; i++){ //looping through every person in the network minus mark
+                if ((subset & (1 << (i - 1))) != 0) { //if their i value is a part of the subset
+                	removedPeople.add(i); //add them to the removal list
                 }
-                if (!valid) break;
+            }
+            
+            //depth-first search (DFS) algorithm
+            boolean valid = true; //initializes a validity variable to see whether a person on the removal list has a valid subset
+            for (int person : removedPeople) { //enhanced looping through every person on the removal list
+                Stack<Integer> stack = new Stack<>(); //creating a stack to hold every person
+                stack.push(person); //pushing the current person to the top of the stack
+                while (!stack.isEmpty()) { //while there are still people in the stack
+                    int current = stack.pop(); //sets current to the person at the top of the stack and removes them from the stack
+                    for (int child : tree[current]) { //looping through every child of the person on the tree
+                        if (!removedPeople.contains(child)) { //if the remove people list does not contain 1 child
+                            valid = false; //set the person to not valid
+                            break; //break the loop
+                        }
+                        stack.push(child); //put the child on the top of the stack
+                    }
+                    if (!valid) { //if the person is not valid
+                    	break; //break the while and continue
+                    }
+                }
+                if (!valid) { //if the person is not valid
+                	break; //break the for loop to stop iterating through all people
+                }
             }
 
-            if (valid) count++;
+            if (valid) { //if the person is valid for removal
+            	count++; //increase the removal count
+            }
         }
 
-        System.out.println(count);
+        System.out.println(count); //print out the counter result
     }
 }
