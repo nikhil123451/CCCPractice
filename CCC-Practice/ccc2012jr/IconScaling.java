1518c68773720c3e7782a90eb71b3249e3b3183f
nikti <nikti@Andy>
2025-07-25 13:46:35 -0600

    works and understood

--- CCC-Practice/ccc2012jr/IconScaling.java
+++ CCC-Practice/ccc2012jr/IconScaling.java
@@ -2,32 +2,29 @@ import java.util.Scanner;
 
 public class IconScaling { //taken from GPT and modified
 	
-    static Scanner scn = new Scanner(System.in);
+    static Scanner scn = new Scanner(System.in); //initializing a scanner
 	
-    public static void main(String[] args) {
-        int k = scn.nextInt();  // Read scaling factor
-        scn.close();
+    public static void main(String[] args) { //main method
+        int scaleFactor = scn.nextInt();  //getting the scaleFactor from input
+        scn.close(); //closing the scanner
+        if (scaleFactor < 25) { //if the input meets input specs
+        	String[] icon = { //the original icon to be scaled
+                    "*x*", //top row
+                    " xx", //middle row
+                    "* *" //bottom row
+            };
 
-        // Original 3x3 icon pattern
-        String[] icon = {
-            "*x*",
-            "xx ",
-            "* *"
-        };
-
-        // Loop through each row in the icon
-        for (String row : icon) {
-            // Build the scaled row
-            StringBuilder scaledRow = new StringBuilder();
-            for (char c : row.toCharArray()) {
-                for (int i = 0; i < k; i++) {
-                    scaledRow.append(c);  // Repeat each character k times
+            for (String row : icon) { //looping through every row in the icon
+                StringBuilder scaledRow = new StringBuilder(); //creating a new string builder to scale up every row
+                for (char character : row.toCharArray()) { //looping through every character in the original row
+                    for (int i = 0; i < scaleFactor; i++) { //repeats scale factor times
+                        scaledRow.append(character); //add the character again
+                    }
                 }
-            }
 
-            // Print the scaled row k times for vertical scaling
-            for (int i = 0; i < k; i++) {
-                System.out.println(scaledRow.toString());
+                for (int i = 0; i < scaleFactor; i++) { //repeats scale factor times
+                    System.out.println(scaledRow.toString()); //print out the scaled row for vertical scaling
+                }
             }
         }
     }
