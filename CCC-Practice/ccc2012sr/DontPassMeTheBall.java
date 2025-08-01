ea8b21ac9b9026f48586cdf80ba0417737ebb154
nikti <nikti@Andy>
2025-08-01 09:17:09 +0530

    works and understood

--- CCC-Practice/ccc2012sr/DontPassMeTheBall.java
+++ CCC-Practice/ccc2012sr/DontPassMeTheBall.java
@@ -2,20 +2,21 @@ import java.util.Scanner;
 
 public class DontPassMeTheBall { //taken from GPT and modified
 	
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int J = sc.nextInt();
-        sc.close();
+	static Scanner scn = new Scanner(System.in); //initializing a scanner
+	
+    public static void main(String[] args) { //main method
+        int jerseyNumber = scn.nextInt(); //getting the jersey number
+        scn.close(); //closing the scanner
         
-        if (J >= 1 && J <= 99) {
-        	if (J < 4) {
-                System.out.println(0);
-                return;
+        if (jerseyNumber >= 1 && jerseyNumber <= 99) { //if the jersey number fits within the restrictions given in the problem
+        	if (jerseyNumber < 4) { //if the jersey number is less than 4
+                System.out.println(0); //print 0 and end the program since there are no possible combinations with a jersey number lower than 4
+                return; //returns void
             }
 
-            long n = J - 1; // total players before scorer
-            long combinations = (n * (n - 1) * (n - 2)) / 6;
-            System.out.println(combinations);
+            long players = jerseyNumber - 1; //number of players before the person who scored
+            long combinations = (players * (players - 1) * (players - 2)) / 6; //combination algorithm for the amount of combinations
+            System.out.println(combinations); //prints out the result
         }
     }
 }
