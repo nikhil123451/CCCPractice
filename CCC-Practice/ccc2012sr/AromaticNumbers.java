94906b069937b06b556b0e62ffa1c1908e915e2e
nikti <nikti@Andy>
2025-08-03 09:36:44 +0700

    works

--- CCC-Practice/ccc2012sr/AromaticNumbers.java
+++ CCC-Practice/ccc2012sr/AromaticNumbers.java
@@ -15,26 +15,43 @@ public class AromaticNumbers {
 		romanNumerals.put("M", 1000);
 		
 		String inputString = scn.nextLine();
+		scn.close();
 		
 		if (inputString.length() >= 2 && inputString.length() <= 20) {
 			String[] symbols = inputString.split("");
 			
 			int result = 0;
 			
-			for (int i = 0 ; i < inputString.length(); i = i + 2) {
-				int arabicDigit = Integer.parseInt(symbols[i]);
-				int romanNumeral = romanNumerals.get(symbols[i+1]);
+			for (int i = 1; i < inputString.length(); i = i + 2) {
+				int arabicNumber = Integer.parseInt(symbols[i - 1]);
+				int romanNumeral = romanNumerals.get(symbols[i]);
 				
-				int previousRomanNumeral = 0;
-				int previousArabicDigit = -1;
+				if (i == 1) {
+					if (inputString.length() == 2) {
+						result = arabicNumber * romanNumeral;
+						System.out.println(result);
+						return;
+					}
+					continue;
+				}
+
+				int previousArabicNumber = Integer.parseInt(symbols[i - 3]);
+				int previousRomanNumeral = romanNumerals.get(symbols[i - 2]);
 				
-				if (!(i == 0)) {
-					previousArabicDigit = Integer.parseInt(symbols[i-2]);
-					previousRomanNumeral = romanNumerals.get(symbols[i-1]);
+				if (romanNumeral > previousRomanNumeral) {
+					result -= previousArabicNumber * previousRomanNumeral;
+				} else if (romanNumeral <= previousRomanNumeral) {
+					result += previousArabicNumber * previousRomanNumeral;
+				} else {
+					return;
 				}
 				
-				int product = arabicDigit * romanNumeral;
+				if (i == inputString.length() - 1) {
+					result += arabicNumber * romanNumeral;
+				}
 			}
+			
+			System.out.println(result);
 		}
 	}
 }
