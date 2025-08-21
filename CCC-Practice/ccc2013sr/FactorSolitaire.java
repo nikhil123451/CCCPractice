8a88464d92544298dac07a06e10468c158bd1dd8
nikti <nikti@Andy>
2025-08-21 08:25:43 -0600

    works and understood

--- CCC-Practice/ccc2013sr/FactorSolitaire.java
+++ CCC-Practice/ccc2013sr/FactorSolitaire.java
@@ -1,41 +1,45 @@
 import java.util.*;
 
-public class FactorSolitaire {
+public class FactorSolitaire { //taken from GPT and modified
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        sc.close();
+	static Scanner scn = new Scanner(System.in); //initializing a scanner
+	
+    public static void main(String[] args) { //main method
+        int targetNumber = scn.nextInt(); //getting the target number to be reached
+        scn.close(); //closing the scanner
+        
+        if (targetNumber >= 1 && targetNumber <= 5000000) { //if the target number fits within the restrictions given by the problem
+        	long[] costs = new long[targetNumber + 1]; //creating an array for every cost of every number (+1 cuz java is 0-indexed)
+            Arrays.fill(costs, Long.MAX_VALUE / 2); //setting each value in the array to a value that's really big before setting the values to reasonable numbers
+            costs[1] = 0; //setting the cost of 1 to be 0
 
-        long[] dp = new long[N + 1];
-        Arrays.fill(dp, Long.MAX_VALUE / 2); // prevent overflow
-        dp[1] = 0;
-
-        // Precompute divisors
-        List<Integer>[] divisors = new ArrayList[N + 1];
-        for (int i = 1; i <= N; i++) {
-            divisors[i] = new ArrayList<>();
-        }
-        for (int i = 1; i <= N; i++) {
-            for (int j = i; j <= N; j += i) {
-                divisors[j].add(i);
+            @SuppressWarnings("unchecked") //suppressing unchecked warnings
+    		List<Integer>[] divisors = new ArrayList[targetNumber + 1]; //creating a list of divisors for every number (+1 cuz java is 0-indexed)
+            for (int i = 1; i <= targetNumber; i++) { //for every number before and including the target number
+                divisors[i] = new ArrayList<>(); //make a new arrayList for every number, which will contain it's divisors
             }
-        }
-
-        // DP transitions
-        for (int c = 1; c < N; c++) {
-            long currCost = dp[c];
-            if (currCost >= Long.MAX_VALUE / 2) continue;
-
-            for (int a : divisors[c]) {
-                int b = c / a;
-                int next = c + a;
-                if (next <= N) {
-                    dp[next] = Math.min(dp[next], currCost + b);
+            for (int i = 1; i <= targetNumber; i++) { //for every number before and including the target number
+                for (int multiple = i; multiple <= targetNumber; multiple += i) { //visiting every multiple of i
+                    divisors[multiple].add(i); //adding i to j's divisor list
                 }
             }
-        }
 
-        System.out.println(dp[N]);
+            for (int currentNumber = 1; currentNumber < targetNumber; currentNumber++) { //looping through each number before the target number
+                long cost = costs[currentNumber]; //getting the current cost of the number
+                if (cost >= Long.MAX_VALUE / 2) { //if the current cost is greater than or equal to the close-to-infinity number we filled into the long[] array
+                	continue; //keep going through the for loop as we haven't found a cost for that number yet
+                }
+
+                for (int divisorA : divisors[currentNumber]) { //looping through every divisor for the current number
+                    int divisorB = currentNumber / divisorA; //getting the other divisor by dividing the current number by divisor A
+                    int nextNumber = currentNumber + divisorA; //setting the next number as current number plus divisor A
+                    if (nextNumber <= targetNumber) { //if the next number is in range
+                        costs[nextNumber] = Math.min(costs[nextNumber], cost + divisorB); //setting the cheapest cost if we've already found the path
+                    }
+                }
+            }
+
+            System.out.println(costs[targetNumber]); //printing out the cost to reach the target number
+        }
     }
 }
