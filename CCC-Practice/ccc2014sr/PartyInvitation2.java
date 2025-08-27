f22ba76e54725ba31fa88aca43abdbfe684172ac
nikti <nikti@Andy>
2025-08-27 09:23:57 -0600

    works

---
+++ CCC-Practice/ccc2014sr/PartyInvitation2.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class PartyInvitation2 {
+	static Scanner scn = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		int friends = scn.nextInt();
+		int rounds = scn.nextInt();
+		
+		ArrayList<Integer> invitations = new ArrayList<>();
+		
+		for (int i = 1 ; i <= friends ; i++) {
+			invitations.add(i);
+		}
+		
+		for (int i = 0 ; i < rounds ; i++) {
+			int multiple = scn.nextInt();
+			
+			for (int j = multiple ; j <= invitations.size() ; j = j + multiple) { //removing the multiples by setting all values that should be removed to 0
+				invitations.set(j-1, 0);
+			}
+			
+			for (int j = 0 ; j < invitations.size(); j++) { //removing all 0s in the araryList
+				if (invitations.get(j) == 0) {
+					invitations.remove(j);
+				}
+			}
+		}
+		
+		for (int friendNumber : invitations) {
+			System.out.println(friendNumber);
+		}
+		
+		scn.close();
+	}
+}
