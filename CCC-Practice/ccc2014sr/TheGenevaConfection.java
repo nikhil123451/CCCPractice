28d316c9a4a2189627b61bc1bdb559de0cbe3eac
nikti <nikti@Andy>
2025-08-30 10:48:19 -0600

    works and understood

--- CCC-Practice/ccc2014sr/TheGenevaConfection.java
+++ CCC-Practice/ccc2014sr/TheGenevaConfection.java
@@ -1,43 +1,47 @@
 import java.util.*;
 
-public class TheGenevaConfection {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        StringBuilder out = new StringBuilder();
+public class TheGenevaConfection { //taken from GPT and modified
+	
+	static Scanner scn = new Scanner(System.in); //initializing a scanner
+	
+    public static void main(String[] args) { //main method
+        StringBuilder output = new StringBuilder(); //creating a stringBuilder for the output
 
-        int T = sc.nextInt();
-        while (T-- > 0) {
-            int N = sc.nextInt();
-            int[] cars = new int[N];
-            for (int i = 0; i < N; i++) {
-                cars[i] = sc.nextInt();
+        int tests = scn.nextInt(); //getting the amount of tests to be run
+        
+        while (tests-- > 0) { //while there are still tests to do, decrement tests after every test
+            int cars = scn.nextInt(); //getting the number of cars
+            int[] carPositions = new int[cars]; //creating an int array for the car positions
+            for (int i = 0; i < cars; i++) { //looping through every car
+                carPositions[i] = scn.nextInt(); //adding the car to the position array
             }
 
-            int[] stack = new int[N];
-            int top = -1;
-            int need = 1;
+            int[] stack = new int[cars]; //creating a stack array for the cars in the branch
+            int topIndex = -1; //index to show cars available in the stack array (starting at -1, cuz java is 0-indexed)
+            int carNeeded = 1; //variable that shows us what car we need (starts at car 1 initially)
+            
+            for (int i = cars - 1; i >= 0; i--) { //looping through cars in reverse order
+                int car = carPositions[i]; //getting the current car
 
-            // process cars in reverse order (bottom → top of input list)
-            for (int i = N - 1; i >= 0; i--) {
-                int car = cars[i];
-
-                if (car == need) {
-                    need++;
-                } else {
-                    stack[++top] = car;
+                if (car == carNeeded) { //if the car is the next one in line
+                    carNeeded++; //set neededCar to the next car needed
+                } else { //if it's not the right car
+                    stack[++topIndex] = car; //set the next index up to the car (adding it to the branch)
                 }
 
-                // flush stack
-                while (top >= 0 && stack[top] == need) {
-                    top--;
-                    need++;
+                while (topIndex >= 0 && stack[topIndex] == carNeeded) { //while there are cars in the branch and the top car is the needed car
+                    topIndex--; //set the top index to the next one down
+                    carNeeded++; //set the needed car to the next one
                 }
             }
 
-            if (need == N + 1) out.append("Y\n");
-            else out.append("N\n");
+            if (carNeeded == cars + 1) { //if needed cars has exceeded the amount of cars
+            	output.append("Y\n"); //set that test to Y because all the cars have been processed properly
+            } else { //if the needed car is still waiting for a car at the top or in the branch
+            	output.append("N\n"); //set that test to N because not all cars have been processed properly
+            }
         }
 
-        System.out.print(out.toString());
+        System.out.print(output.toString()); //print out the output accumulated
     }
 }
