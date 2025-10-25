602d595f24d56727b3fee7e8369a2875822d6f98
nikti <nikti@Andy>
2025-10-24 21:43:43 -0600

    works and understood

--- CCC-Practice/ccc2017jr/NailedIt.java
+++ CCC-Practice/ccc2017jr/NailedIt.java
@@ -1,43 +1,48 @@
 import java.util.*;
 
 public class NailedIt { //taken from GPT and modified
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[] freq = new int[2001]; // wood lengths range from 1 to 2000
+	
+	static Scanner scn = new Scanner(System.in); //initializing a scanner
+	static final int MAXIMUM_WOOD_LENGTH = 2000;
+	static final int MINIMUM_WOOD_LENGTH = 1;
+	
+    public static void main(String[] args) { //main method
+        int woodPieces = scn.nextInt(); //getting the amount of wood pieces
+        int[] frequencies = new int[MAXIMUM_WOOD_LENGTH + 1]; //creating a frequency array for every wood length (+1 cuz java is 0-indexed)
 
-        for (int i = 0; i < N; i++) {
-            int length = sc.nextInt();
-            freq[length]++;
+        for (int i = 0; i < woodPieces; i++) { //looping through every wood piece
+            int length = scn.nextInt(); //get its length
+            frequencies[length]++; //increment the length in the frequency array
         }
+        scn.close(); //closing the scanner
 
-        int maxFenceLength = 0;      // number of boards in the longest fence
-        int numMaxHeights = 0;       // number of different heights achieving that maximum
+        int maximumFenceLength = 0; //variable to keep track of the longest fence length
+        int maximumFenceHeights = 0; //variable to keep track of the possible heights given a fence length
+        
+        for (int sumOfPieces = 2 * MINIMUM_WOOD_LENGTH; sumOfPieces <= 2 * MAXIMUM_WOOD_LENGTH; sumOfPieces++) { //looping through each possible sum of wood lengths
+            int boards = 0; //variable to keep track of the boards that can be made
 
-        // possible board lengths go from 2 to 4000 (sum of two pieces)
-        for (int s = 2; s <= 4000; s++) {
-            int boards = 0;
+            for (int i = MINIMUM_WOOD_LENGTH; i <= sumOfPieces / 2 && i <= MAXIMUM_WOOD_LENGTH; i++) { //looping through individual wood piece lengths
+                int j = sumOfPieces - i; //getting the other pair that goes with piece i
+                if (j < MINIMUM_WOOD_LENGTH || j > MAXIMUM_WOOD_LENGTH) { //if j does not fit the bounds of a wood piece
+                	continue; //keep going through the loop
+                }
 
-            // combine pairs that sum to s
-            for (int i = 1; i <= s / 2 && i <= 2000; i++) {
-                int j = s - i;
-                if (j < 1 || j > 2000) continue;
-
-                if (i == j) {
-                    boards += freq[i] / 2;
+                if (i == j) { //if the 2 pieces are of equal length
+                    boards += frequencies[i] / 2; //getting the number of boards made with 2 pieces
                 } else {
-                    boards += Math.min(freq[i], freq[j]);
+                    boards += Math.min(frequencies[i], frequencies[j]); //getting the number of boards made by getting the frequency of the material not in access
                 }
             }
 
-            if (boards > maxFenceLength) {
-                maxFenceLength = boards;
-                numMaxHeights = 1;
-            } else if (boards == maxFenceLength && boards > 0) {
-                numMaxHeights++;
+            if (boards > maximumFenceLength) { //if the board count is greater than the recorded maximum
+                maximumFenceLength = boards; //set the new maximum as the amount of boards
+                maximumFenceHeights = 1; //only 1 board height achieved this record
+            } else if (boards == maximumFenceLength && boards > 0) { //if the board amount is equal to the current maximum and it's a positive integer
+                maximumFenceHeights++; //increase the amount of board heights that made it to the maximum
             }
         }
 
-        System.out.println(maxFenceLength + " " + numMaxHeights);
+        System.out.println(maximumFenceLength + " " + maximumFenceHeights); //print out the result
     }
 }
