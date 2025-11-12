ce44f579af7123c3a68a82c3e4d430926e92f829
nikti <nikti@Andy>
2025-11-12 15:18:16 -0700

    works and understood

--- CCC-Practice/ccc2018sr/Sunflowers.java
+++ CCC-Practice/ccc2018sr/Sunflowers.java
@@ -1,52 +1,57 @@
 import java.util.*;
 
 public class Sunflowers { //taken from GPT and modified
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int[][] grid = new int[N][N];
+	
+	static Scanner scn = new Scanner(System.in); //initializing a scanner
+	
+    public static void main(String[] args) { //main method
+        
+        int flowers = scn.nextInt(); //getting the number of sunflowers
+        int[][] grid = new int[flowers][flowers]; //creating the grid of flower data
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                grid[i][j] = sc.nextInt();
+        for (int i = 0; i < flowers; i++) { //looping through every row in the grid
+            for (int j = 0; j < flowers; j++) { //looping through every column
+                grid[i][j] = scn.nextInt(); //setting the coordinate appropriately
             }
         }
 
-        // Rotate until sorted properly
-        while (!isSorted(grid, N)) {
-            grid = rotate(grid, N);
+        while (!isSorted(grid, flowers)) { //while the grid is not sorted properly, as per the problem specifications
+            grid = rotate(grid, flowers); //rotate the grid
         }
 
-        // Print the correct orientation
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                System.out.print(grid[i][j] + " ");
+        for (int i = 0; i < flowers; i++) { //looping through every row
+            for (int j = 0; j < flowers; j++) { //looping through every column
+                System.out.print(grid[i][j] + " "); //printing out the coordinate plus a space
             }
-            System.out.println();
+            System.out.println(); //going down the the next row
         }
     }
 
-    static boolean isSorted(int[][] grid, int N) {
-        for (int i = 0; i < N; i++) {
-            for (int j = 1; j < N; j++) {
-                if (grid[i][j] <= grid[i][j - 1]) return false; // not increasing in row
+    static boolean isSorted(int[][] grid, int flowers) { //helper method to check if the given grid is sorted with a flower count
+        for (int i = 0; i < flowers; i++) { //looping through every row
+            for (int j = 1; j < flowers; j++) { //looping through every column but the first one
+                if (grid[i][j] <= grid[i][j - 1]) { //if the value in the column in front is less than the value in the column before it
+                	return false; //return false because they aren't increasing values
+                }
             }
         }
-        for (int i = 1; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                if (grid[i][j] <= grid[i - 1][j]) return false; // not increasing in column
+        for (int i = 1; i < flowers; i++) { //looping through every row but the first one
+            for (int j = 0; j < flowers; j++) { //looping through every column
+                if (grid[i][j] <= grid[i - 1][j]) { //if the value in the current row is less than the value of the previous row
+                	return false; //return false because they aren't increasing values
+                }
             }
         }
-        return true;
+        return true; //return true if all values are increasing
     }
 
-    static int[][] rotate(int[][] grid, int N) {
-        int[][] newGrid = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                newGrid[i][j] = grid[N - j - 1][i];
+    static int[][] rotate(int[][] grid, int flowers) { //method to "rotate" the given grid
+        int[][] newGrid = new int[flowers][flowers]; //creating a new grid to place all the new values
+        for (int i = 0; i < flowers; i++) { //looping through every row
+            for (int j = 0; j < flowers; j++) { //looping through every column
+                newGrid[i][j] = grid[flowers - j - 1][i]; //rotation algorithm to rotate clockwise 90 degrees
             }
         }
-        return newGrid;
+        return newGrid; //return the final grid after rotation
     }
 }
