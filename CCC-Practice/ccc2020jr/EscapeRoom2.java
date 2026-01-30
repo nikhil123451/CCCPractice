e2ccb1e392c9113fb58e65f72be705c3b43c5a6a
nikti <nikti@Andy>
2026-01-30 12:11:49 -0700

    works

---
+++ CCC-Practice/ccc2020jr/EscapeRoom2.java
@@ -0,0 +1,67 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EscapeRoom2 {
+	static Scanner scn = new Scanner(System.in);
+	
+	static int[][] grid;
+	static int rows;
+	static int columns;
+	static boolean[][] checked;
+	static boolean foundPath = false;
+	
+	public static void main(String[] args) {
+		rows = scn.nextInt();
+		columns = scn.nextInt();
+		
+		grid = new int[rows + 1][columns + 1]; //+1 cuz java is 0-indexed
+		checked = new boolean[rows + 1][columns + 1]; //+1 cuz java is 0-indexed
+		
+		for (int i = 1 ; i < grid.length ; i++) {
+			for (int j = 1 ; j < grid[i].length ; j++) {
+				grid[i][j] = scn.nextInt();
+			}
+		}
+		scn.close();
+		
+		findPath(1,1); //starting at (1,1)
+		if (foundPath) {
+			System.out.println("yes");
+		} else {
+			System.out.println("no");
+		}
+	}
+	
+	public static void findPath(int startingRow, int startingColumn) {
+		if (startingRow == rows && startingColumn == columns) {
+			foundPath = true;
+			return;
+		}
+		
+		int value = grid[startingRow][startingColumn];
+		List<int[]> pairs = getFactorPairs(value);
+		
+		for (int[] pair : pairs) {
+			int pairRow = pair[0];
+			int pairColumn = pair[1];
+			
+			if (pairRow <= rows && pairColumn <= columns && !checked[pairRow][pairColumn]) {
+				checked[pairRow][pairColumn] = true;
+				findPath(pairRow, pairColumn);
+			}
+		}
+	}
+	
+	public static List<int[]> getFactorPairs(int number) {
+        List<int[]> pairs = new ArrayList<>();
+
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                pairs.add(new int[]{i, number / i});
+            }
+        }
+
+        return pairs;
+    }
+}
