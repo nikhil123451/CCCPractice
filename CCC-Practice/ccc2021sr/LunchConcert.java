47d95d0f29461ec93a33a0ac6b7da457a427f785
nikti <nikti@Andy>
2026-02-16 12:41:42 -0700

    works and understood

--- CCC-Practice/ccc2021sr/LunchConcert.java
+++ CCC-Practice/ccc2021sr/LunchConcert.java
@@ -1,63 +1,63 @@
 import java.util.*;
 
-public class LunchConcert {
+public class LunchConcert { //taken from GPT and modified
 
-    static Scanner scn = new Scanner(System.in);
+    static Scanner scn = new Scanner(System.in); //initializing a scanner
 
-    static int N;
-    static long[] P, W, D;
+    static int friends; //variable for the amount of friends you have
+    static long[] positions, speeds, hearingRanges; //arrays for the friends' positions, speeds, and hearing ranges
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { //main method
 
-        N = scn.nextInt();
+        friends = scn.nextInt(); //getting the amount of friends
 
-        P = new long[N];
-        W = new long[N];
-        D = new long[N];
+        positions = new long[friends]; //initializing the positions array
+        speeds = new long[friends]; //initializing the speeds array
+        hearingRanges = new long[friends]; //initializing the hearing ranges array
 
-        long left = Long.MAX_VALUE;
-        long right = Long.MIN_VALUE;
+        long left = Long.MAX_VALUE; //setting left as an impossibly large value
+        long right = Long.MIN_VALUE; //setting right as an impossibly small value
 
-        for (int i = 0; i < N; i++) {
-            P[i] = scn.nextLong();
-            W[i] = scn.nextLong();
-            D[i] = scn.nextLong();
+        for (int i = 0; i < friends; i++) { //for every friend
+            positions[i] = scn.nextLong(); //set their position
+            speeds[i] = scn.nextLong(); //set their speed
+            hearingRanges[i] = scn.nextLong(); //set their hearing range
 
-            left = Math.min(left, P[i] - D[i]);
-            right = Math.max(right, P[i] + D[i]);
+            left = Math.min(left, positions[i] - hearingRanges[i]); //getting left-most bound
+            right = Math.max(right, positions[i] + hearingRanges[i]); //getting the right-most bound
         }
 
-        // Ternary search on convex function
-        while (right - left > 3) {
-            long m1 = left + (right - left) / 3;
-            long m2 = right - (right - left) / 3;
+        while (right - left > 3) { //while the bounds are large enough to keep shrinking
+            long midpoint1 = left + (right - left) / 3; //get the first midpoint between the bounds
+            long midpoint2 = right - (right - left) / 3; //get the second midpoint between the bounds
 
-            if (cost(m1) <= cost(m2)) {
-                right = m2 - 1;
-            } else {
-                left = m1 + 1;
+            if (cost(midpoint1) <= cost(midpoint2)) { //if the value of midpoint1 is less than or equal to the value of midpoint2
+                right = midpoint2 - 1; //set the new right bound accordingly
+            } else { //if the value of midpoint1 is actually greater than the value of midpoint2
+                left = midpoint1 + 1; //set the new left bound accordingly
             }
         }
 
-        long answer = Long.MAX_VALUE;
+        long result = Long.MAX_VALUE; //setting the result to an arbitrarily large value
 
-        for (long c = left; c <= right; c++) {
-            answer = Math.min(answer, cost(c));
+        for (long value = left; value <= right; value++) { //looping through the length of the bounds
+            result = Math.min(result, cost(value)); //set the minimum result accordingly
         }
 
-        System.out.println(answer);
+        System.out.println(result); //print out the result
+        scn.close(); //closing the scanner
     }
 
-    static long cost(long c) {
-        long total = 0;
+    static long cost(long cValue) { //helper method that finds the speed of all friends traversing to point c
+        long total = 0; //total counter
 
-        for (int i = 0; i < N; i++) {
-            long dist = Math.abs(P[i] - c) - D[i];
-            if (dist > 0) {
-                total += dist * W[i];
+        for (int i = 0; i < friends; i++) { //looping through every friend
+            long dist = Math.abs(positions[i] - cValue) - hearingRanges[i]; //get the friend's distance from c (accounting for their hear
+            if (dist > 0) { //if the distance is non-negative
+                total += dist * speeds[i]; //increment the total time accordingly
             }
         }
 
-        return total;
+        return total; //return the total time calculated
     }
 }
